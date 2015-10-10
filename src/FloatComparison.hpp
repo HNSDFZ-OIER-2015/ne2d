3af267fad2f126e6d0c21bb3017bc7aaa74c7fad
riteme <1412803389@qq.com>
2015-10-10 20:28:30 +0800

    Add noexcept for FloatComaprsion

--- src/FloatComparison.hpp
+++ src/FloatComparison.hpp
@@ -11,27 +11,27 @@
 namespace ne {
 
 template <typename TFloat>
-bool IsSame(const TFloat a, const TFloat b) {
+bool IsSame(const TFloat a, const TFloat b) noexcept {
     return fabs(a - b) < std::numeric_limits<TFloat>::epsilon();
 }
 
 template <typename TFloat>
-bool IsGreater(const TFloat a, const TFloat b) {
+bool IsGreater(const TFloat a, const TFloat b) noexcept {
     return a > b && !IsSame(a, b);
 }
 
 template <typename TFloat>
-bool IsLess(const TFloat a, const TFloat b) {
+bool IsLess(const TFloat a, const TFloat b) noexcept {
     return a < b && !IsSame(a, b);
 }
 
 template <typename TFloat>
-bool IsGreaterEqual(const TFloat a, const TFloat b) {
+bool IsGreaterEqual(const TFloat a, const TFloat b) noexcept {
     return a > b || IsSame(a, b);
 }
 
 template <typename TFloat>
-bool IsLessEqual(const TFloat a, const TFloat b) {
+bool IsLessEqual(const TFloat a, const TFloat b) noexcept {
     return a < b || IsSame(a, b);
 }
 
